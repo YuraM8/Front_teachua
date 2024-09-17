d11a631eca9f6902adb6f4583294d3fc19bdd759
YuraM8 <127991717+YuraM8@users.noreply.github.com>
2024-09-18 00:36:45 +0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(antd|@babel)/)', // Ігнорування node_modules, окрім antd та babel
-  ],
-};
