2fe2d2eca23ce8e31e365fb4b0a8372082181bee
YuraM8 <127991717+YuraM8@users.noreply.github.com>
2024-09-18 00:00:16 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders HeaderComponent correctly', () => {
   render(
       <App />
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByRole('banner');
+  expect(headerElement).toBeInTheDocument();
 });
